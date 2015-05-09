888a407141679cd987ae5bdf5923b7a1b8a4e960
Arthur Maltson <arthur@maltson.com>
2015-05-09 18:58:31 -0400

    :sunrise:
    Generated with:
    chef generate cookbook test-driven-redis

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'test-driven-redis'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures test-driven-redis'
+long_description 'Installs/Configures test-driven-redis'
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: test-driven-redis
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
