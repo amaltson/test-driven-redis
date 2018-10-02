e4af1efd20805b8e06d6430468cde4bcb29dcaa1
Arthur Maltson <arthur@maltson.com>
2018-10-02 06:55:28 -0400

    Use redisio cookbook from Supermarket
    As an easy out of the box way to install Redis, we can use the redisio
    cookbook found on at https://supermarket.chef.io/cookbooks/redisio/

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,7 @@ long_description 'Installs/Configures test-driven-redis'
 version '0.1.0'
 chef_version '>= 13.0'
 
+depends 'redisio'
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
