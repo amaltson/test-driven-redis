36e0c4601c25645bd95ff6518b5abcf71ba065e0
Arthur Maltson <arthur@maltson.com>
2020-05-20 00:04:21 -0400

    :rotating_light: Write first failing test
    The first failing test will check if the Redis user and the directory
    it stores it's database exist. We haven't setup anything for Redis yet,
    so we expect this to fail.

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -3,14 +3,10 @@
 # The InSpec reference, with examples and extensive documentation, can be
 # found at https://www.inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+describe user('redis') do
+  it { should exist }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe file('/var/lib/redis') do
+  it { should be_directory }
 end
