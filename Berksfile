8064f0d5f548b9681958aa330c4a04252a1f5e9c
Arthur Maltson <arthur@maltson.com>
2020-05-19 23:48:12 -0400

    :fire: Clear all assets for ChefConf
    Going to regenerate and build up the cookbook again for ChefConf.

--- Berksfile
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-source 'https://supermarket.chef.io'
-
-metadata
-
-cookbook 'redisio', '~> 2.7'
--- recipes/default.rb
+++
@@ -1,7 +0,0 @@
-#
-# Cookbook:: test-driven-redis
-# Recipe:: default
-#
-# Copyright:: 2018, The Authors, All Rights Reserved.
-include_recipe 'redisio::default'
-include_recipe 'redisio::enable'
